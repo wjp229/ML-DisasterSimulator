69a589b5bdfb7737c7d25f0a81d4c611320cc4f2
Do Hyung Kim <wjp229@naver.com>
2023-05-30 14:13:43 +0900

    Update Scene

--- Assets/02. Scripts/ResultUI.cs
+++ Assets/02. Scripts/ResultUI.cs
@@ -11,11 +11,11 @@ public class ResultUI : MonoBehaviour
     public void SetResult(ResultInfo info)
     {
         textContext.text =
-            "Init Escape Time : " + info.InitEscapeTime + "\n" +
-            "Average Escape Time : " + info.AvgEscapeTime + "\n" +
-            "Last Escape Time : " + info.LastEscapeTime + "\n" +
+            "Init Escape Time : " + info.InitEscapeTime.ToString("F1") + " sec\n" +
+            "Average Escape Time : " + info.AvgEscapeTime.ToString("F1") + " sec\n" +
+            "Last Escape Time : " + info.LastEscapeTime.ToString("F1") + " sec\n" +
             "Death Count : " + info.DeathCnt + "\n" +
-            "Death Rate : " + info.DeathRate.ToString("F1");
+            "Death Rate : " + Mathf.Floor(info.DeathRate * 30) +"%";
     }
 
     public void RestartScene()

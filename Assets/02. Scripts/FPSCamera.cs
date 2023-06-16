ce161b320cd50d8b884cf8c9b746f9812ed9b4c7
kimyoorym <61074663+kimyoorym@users.noreply.github.com>
2023-06-16 16:23:59 +0900

    FPSCamera 추가
    eulerAngels 활용 코드에서 오류 1개 발생(eulerAngels 인식 안됨)

---
+++ Assets/02. Scripts/FPSCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour
+{
+    private float mouseSensivility = 100f;
+    private float rotY;
+    private float rotX;
+    private float rotXLimit = 80f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngels;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * Time.deltaTime * mouseSensivility;
+        rotX += mouseY * Time.deltaTime * mouseSensivility;
+
+        rotX = Mathf.Clamp(rotX, -rotXLimit, rotXLimit);
+
+        Quaternion mouseRot = Quaternion.Euler(rotX, rotY, 0);
+        transform.localRotation = mouseRot;
+    }
+}

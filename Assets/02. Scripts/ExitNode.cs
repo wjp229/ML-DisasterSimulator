9883048dede133aae39ed877c9c8143382fe486f
Mandorica <44854660+Mandorica@users.noreply.github.com>
2023-06-19 01:02:40 +0900

    최종변경

--- Assets/02. Scripts/ExitNode.cs
+++ Assets/02. Scripts/ExitNode.cs
@@ -5,5 +5,22 @@ using UnityEngine;
 
 public class ExitNode : MonoBehaviour
 {
-    
+    /*public float minReward = 0.05f;
+    public float maxReward = .5f;
+
+    public float radius = 10f;
+
+    public SimulatorAgent _agent;
+
+    private void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, 1<<LayerMask.NameToLayer("Escapee"));
+        int i = 0;
+        float rewardRes = 0;
+        while (i < hitColliders.Length) {
+            rewardRes += Mathf.Lerp(minReward, maxReward, Vector3.Distance(transform.position, hitColliders[i].transform.position) / radius);
+        }
+
+        _agent.AddReward(rewardRes);
+        */
 }
